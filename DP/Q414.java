e6a23700a3dd5e055b7c40b95e5916e1069b86e9
Pranay <pranayprasad42@gmail.com>
2022-05-05 18:57:23 +0530

    DP

---
+++ DP/Q414.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Q414{
+    public static int solve(int n){
+        int [] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n;i++){
+            dp[i] = 0;
+            for(int j = 0; j < i;j++){
+                dp[i] += dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = solve(n);
+        sc.close();
+        System.out.println(ans);
+    }
+}
